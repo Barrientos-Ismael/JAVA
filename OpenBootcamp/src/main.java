34daf9b42e24a3061843c22e9c2fac65910a65a3
Julio Ismael Barrientos 201100133 <ismaeljb92@gmail.com>
2022-07-08 16:16:14 -0300

    NewProject
    Openbootcamp

---
+++ OpenBootcamp/src/main.java
@@ -0,0 +1,32 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("-- PARTE 1 ---");
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        System.out.println(suma(a,b,c));
+
+        System.out.println("-- PARTE 2 ---");
+
+        Coche automovil = new Coche();
+        automovil.incrementarPuertas();
+        System.out.println(automovil.getPuertas());
+
+    }
+
+    public static int suma(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+}
+
+class Coche {
+        private int numPuertas = 0;
+
+        public void incrementarPuertas(){
+            this.numPuertas++;
+        }
+
+        public int getPuertas(){
+            return numPuertas;
+        }
+}
